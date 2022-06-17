844bcc6fa8917acbf15541bd0f0c64e0d067d697
HoangTran <trannhathoang231@gmail.com>
2022-06-17 14:56:55 +0700

    Enter

--- js/ProductList.js
+++ js/ProductList.js
@@ -1,8 +1,27 @@
 function ProductList(){
-    this.mangPr = [];
+    this.getList = function () {
+        // const promise = axios
+        return axios({
+            method: 'https://62a2fc9e21232ff9b2151283.mockapi.io/Products_Capstone'
+        })
 
-    this.themPr = function(pr){
-        this.mangPr.push(pr);
+        // return promise;
+    }
+
+    this.deleteItem = function (id) {
+        return axios({
+            method: 'delete',
+            url: `https://62a2fc9e21232ff9b2151283.mockapi.io/Products_Capstone/${id}`
+        })
+    }
+
+    this.addItem = function (sp) {
+        // data: loại dữ liệu đối tượng
+        return axios({
+            method: 'post',
+            url: 'https://62a2fc9e21232ff9b2151283.mockapi.io/Products_Capstone',
+            data: sp
+        });
     }
     
 }
