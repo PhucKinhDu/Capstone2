02e1fb4a65350e04bff489027d1ca263b0e3f1a5
Tinh <lamhuynhthientinh@gmail.com>
2022-06-17 23:28:45 +0700

    update Axios JS

--- app/models/SanPham.js
+++ app/models/SanPham.js
@@ -1,9 +1,13 @@
 
 
-function SanPham(ten,gia,hinh,mota)
+function SanPham(ten,gia,manHinh,camSau,camTruoc,hinh,mota,loai)
 {
   this.name = ten;
   this.price = gia;
+  this.screen = manHinh;
+  this.backCamera = camSau;
+  this.frontCamera = camTruoc;
   this.img = hinh;
   this.desc = mota;
+  this.type = loai;
 }
--- app/services/SanPhamService.js
+++ app/services/SanPhamService.js
@@ -15,9 +15,35 @@ function SanPhamService() {
         });
     }
 
+    this.deleteItem = function(id) {
+        return axios({
+            method: "delete",
+            url: `https://62a2fbb921232ff9b214f77e.mockapi.io/Capstone2/${id}`
+        });
+    }
+    
+    this.getProductItem = function(id) {
+        return axios({
+            method: "get",
+            url: `https://62a2fbb921232ff9b214f77e.mockapi.io/Capstone2/${id}`
+        });
+    }
 
-    // Axios P1 - 1:51:10
+    this.updateProduct = function(id, sp) {
+        return axios({
+            method: "put",
+            url: `https://62a2fbb921232ff9b214f77e.mockapi.io/Capstone2/${id}`,
+            data: sp
+        });
+    }
+
+
 
 
 
 }
+
+
+// Hướng dẫn làm bài Capstone. P2 - 2:30:00
+
+// Qua tới ES6: P2 - 2:38:20
